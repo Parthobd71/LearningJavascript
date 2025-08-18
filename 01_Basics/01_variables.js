11b3c2e0df862b5c5b085b1b749a36088f61aa05
Parthobd71 <99398523+Parthobd71@users.noreply.github.com>
2025-08-18 14:41:27 +0600

    Add files via upload
    ok

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 1234567890 //const variable cannot be changed
+let accountEmail = "partho@google.com"
+accountCity = "dhaka"
+var location = "bangladesh"
+
+accountEmail = "partho@gmail.com"  //you and change the value of let variable   
+accountCity = "chittagong"  //you can change the value of a variable declared with var
+
+
+console.log(accountCity)
+console.log(accountId, accountEmail, accountCity);
